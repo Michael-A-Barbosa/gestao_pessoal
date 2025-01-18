317d7f159a8048bd40bc702c7ae0f59c3a3bd74b
Michael-A-Barbosa <michaeldkml@gmail.com>
2025-01-18 12:28:27 -0300

    commit inicial - configuração básica do projeto

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div id="app" class="container mt-5">
+        <h1 class="text-primary">Gestão Pessoal</h1>
+        <button class="btn btn-success">Exemplo de Botão</button>
+
+        <h1><i class="fa fa-check-circle"></i> Ícone de Check</h1>
+        <button class="btn btn-primary">
+            <i class="fa fa-thumbs-up"></i> Curtir
+        </button>
+    </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
-  name: "App",
-  components: {
-    HelloWorld,
-  },
+    name: 'App'
 };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+/* estilos personalizados aqui, se necessário */
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,6 @@
-import { createApp } from "vue";
-import App from "./App.vue";
+import { createApp } from 'vue';
+import App from './App.vue';
+import '@/styles/main.scss';
+import 'font-awesome/css/font-awesome.css';
 
-createApp(App).mount("#app");
+createApp(App).mount('#app');
